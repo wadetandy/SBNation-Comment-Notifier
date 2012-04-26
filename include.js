10746ff9ff277dfe0a77789e478afcbe2aba534e
Wade Tandy <wade.tandy@gmail.com>
2012-04-26 17:49:32 -0300

    move all of the notification logic out of the included file.  Only handles new comment detection.

--- include.js
+++ include.js
@@ -1,66 +1,19 @@
-var Notification = {
-    Window: window,
-    ShouldShow: false,
-    OnClick: function(e){
-        Notification.Window.focus();
-        Notification.Active = undefined;
-
-        e.target.cancel();
-    },
-    OnError: function(e){
-        console.log('there was an error with this notification');
-    },
-    Icon: function(){
-        return $$('#logo img')[0].src;
-    },
-    Title: function(){
-        return $$('#logo img')[0].alt;
-    },
-    Body: function(){
-        var thread_title = document.title;
-        var blog_title = ' - ' + Notification.Title();
-        
-        thread_title = thread_title.substr(0, thread_title.indexOf(blog_title));
-        return 'New Comments on ' + thread_title;
-    },
-    ReplaceID: function(){
-        return Notification.Window.location.href;
-    },
-    CreateAndShow: function(){
-        if(!Notification.ShouldShow){
-            return;
-        }
-        
-        Notification.Active = webkitNotifications.createNotification(Notification.Icon(), Notification.Title(), Notification.Body());
-
-        Notification.Active.onclick = Notification.OnClick;
-        Notification.Active.onerror = Notification.OnError;
-        Notification.Active.replaceId = Notification.ReplaceID();
-
-        Notification.Active.show();
-    },
-}
-
-Notification.Window.onblur = function(){
-    Notification.ShouldShow = true;
-}
-
-Notification.Window.onfocus = function(){
-    Notification.ShouldShow = false;
-    
-    if(Notification.Active != undefined){
-        Notification.Active.cancel();
-        Notification.Active = undefined;
-    }
-}
-
 Effect.OriginalAppear = Effect.Appear;
 
 Effect.Appear = function(item) {
     if(item == 'autoupdate_info'){
-        $('chrome-notification-plugin').innerHTML = new Date().getTime();
-        //Notification.CreateAndShow();
+        var event = document.createEvent('Event');
+        var eventDiv = document.getElementById('wtandy-notification-plugin');
+
+        event.initEvent('NewCommentsEvent', true, true);
+        eventDiv.dispatchEvent(event);
     }
 
     return Effect.OriginalAppear(item);
 }
+
+document.getElementById('wtandy-notification-plugin').addEventListener('WindowFocusEvent', function() {
+  console.log('window focus event');
+  window.focus();
+});
+
