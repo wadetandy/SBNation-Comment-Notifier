92280f302d46a98519f696fe8f923db2071f18d0
Wade Tandy <wade.tandy@gmail.com>
2012-04-26 17:44:02 -0300

    Commented out chunk at the bottom will go into background.js when I can get back to a machine with commit access.

--- code.js
+++ code.js
@@ -115,3 +115,47 @@ SBNation.Notification = {
 }
 
 SBNation.Setup();
+
+/*
+var SBNation = {};
+
+SBNation.Notification = {
+    Active: {},
+    OnError: function(e){
+        console.log('there was an error with this notification');
+    },
+
+    CreateAndShow: function(icon, title, body, replace_id, source_tab){
+        SBNation.Notification.Active[source_tab.id] = webkitNotifications.createNotification(icon, title, body);
+
+        SBNation.Notification.Active.onclick = function(e){
+            chrome.tabs.sendRequest(source_tab.id, {type: "NotificationClick"});
+            e.target.cancel();
+            SBNation.Notification.Active[source_tab.id] = undefined;
+        };
+
+        SBNation.Notification.Active[source_tab.id].onerror = SBNation.Notification.OnError;
+        SBNation.Notification.Active[source_tab.id].replaceId = replace_id;
+
+        SBNation.Notification.Active[source_tab.id].show();
+    },
+}
+
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    console.log(request.type);
+    if (request.type == "CreateNotification"){
+        SBNation.Notification.CreateAndShow(request.icon, request.title, request.body, request.replace_id, sender.tab);
+        sendResponse({status: "OK"});
+    }
+    else if (request.type == "HideNotification"){
+        SBNation.Notification.Active[source_tab.id].cancel();
+        SBNation.Notification.Active[source_tab.id] = undefined;
+
+        sendResponse({status: "OK"});
+    }
+    else {
+        sendResponse({status: "Invalid Request Type"})
+    }
+});
+*/
